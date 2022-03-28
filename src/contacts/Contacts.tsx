eb65df5c9259838cd91cdd3dd9fc46f7b7c9c775
Rombeso <larionovra@gmail.com>
2022-03-28 18:25:22 +0300

    Added node.js server for send message from form.

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, {ChangeEvent, ChangeEventHandler, FormEvent, useState} from "react";
 import style from './Contacts.module.scss'
 import styleContainer from "../commond/styles/Container.module.css";
 import {Title} from "../commond/components/title/Title";
 import {ContactBlock} from "../commond/components/contactBlock/contactBlock";
 import {faPhone, faAt, faPaperPlane} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 
 export const Contacts = () => {
 
+    const [state, setState] = useState({})
+
+
+    const handleInputChange = (e: (ChangeEvent<HTMLInputElement & HTMLTextAreaElement>)) => {
+        // e.preventDefault()
+        //   const target = e.;
+        const value = e.currentTarget.value;
+        const name = e.currentTarget.name;
+
+        setState({
+            ...state,
+            [name]: value
+        });
+        console.log(state)
+    }
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(state)
+        axios.post("https://smtp-server-myportfolio.herokuapp.com/sendMessage", state)
+            .then(() => {
+                alert('Your massage has been send')
+            })
+    }
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
@@ -22,10 +46,13 @@ export const Contacts = () => {
                         </div>
                     </div>
                     <div className={style.formContacts}>
-                        <form action={''} className={style.form}>
-                            <input type="text" name="name" placeholder={'Name:'} className={style.input}/>
-                            <input type="text" name="company" placeholder={'Company:'} className={style.input}/>
-                            <textarea placeholder={'About your offer:'} className={style.textarea}></textarea>
+                        <form action={''} className={style.form} onSubmit={handleSubmit}>
+                            <input onChange={handleInputChange} type="text" name="name" placeholder={'Name:'}
+                                   className={style.input}/>
+                            <input onChange={handleInputChange} type="text" name="company" placeholder={'Company:'}
+                                   className={style.input}/>
+                            <textarea onChange={handleInputChange} name="message" placeholder={'About offer and your contacts:'}
+                                      className={style.textarea}></textarea>
                             <button className={style.button}>SUBMIT NOW</button>
                         </form>
                     </div>
